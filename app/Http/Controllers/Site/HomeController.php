2f83399fc825e3963c3a58b63c68f04f20460b54
Lançamentos Online <web@lancamentosonline.com.br>
2022-02-14 10:51:32 -0400

    push rezando pra não dar pau

---
+++ app/Http/Controllers/Site/HomeController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Construtora;
+use App\Models\Empreendimento;
+use App\Models\Newsletter;
+use App\Models\Publicacao;
+use App\Http\Requests\NewsletterRequest;
+
+class HomeController extends Controller
+{
+    private $viewHome;
+    private $viewConstrutora;
+
+    public function __construct()
+    {
+        $this->viewHome = isMobile() ? 'site.home.mobile.index' : 'site.home.desktop.index';
+        $this->viewConstrutora = isMobile() ? 'site.construtora.mobile.index' : 'site.construtora.desktop.index';
+    }
+
+    public function index()
+    {
+        $this->data['construtoras'] = Construtora::where('status', 'Liberada')->get()->all();
+        $this->data['apartamentos'] = Empreendimento::where('subtipo_id', 1)->where('status', 'Liberada')->get()->count();
+        $this->data['condominios'] = Empreendimento::where('subtipo_id', 3)->where('status', 'Liberada')->get()->count();
+        $this->data['salas'] = Empreendimento::where('subtipo_id', 2)->where('status', 'Liberada')->get()->count();
+        $this->data['lotes'] = Empreendimento::where('subtipo_id', 4)->where('status', 'Liberada')->get()->count();
+        $this->data['noticias'] = Publicacao::where('status', 'Liberada')->orderBy('data', 'DESC')->take(4)->get();
+
+        return view($this->viewHome, $this->data);
+    }
+
+    public function construtora(Request $request, $construtora, $id)
+    {        
+        $request->request->set('construtora_id', $id);
+        $this->data = (new BuscaController())->getDadosBusca($request, [
+            'url' => "/construtora/{$construtora}-{$id}.html"
+        ]);
+        $this->data['construtora'] = Construtora::find($id);
+
+        return view($this->viewConstrutora, $this->data);
+    }
+
+    public function termosUso()
+    {
+        return view('site.termos_uso/index');
+    }
+
+    public function politicaPrivacidade()
+    {
+        return view('site.politica_privacidade.index');
+    }
+
+    public function newsletter(NewsletterRequest $request)
+    {        
+        $resultado = (new Newsletter())->salvar($request);
+
+        $sucesso = 'false';
+
+        if ($resultado) {
+            $sucesso = 'true';
+        }
+
+        return response()->json([
+            'sucesso' => $sucesso,
+            'retorno' => 'E-mail salvo com sucesso'
+        ]);
+    }
+
+    public function SiteMap()
+	{
+       $empreendimentos = Empreendimento::latest()->where('status', 'Liberada')->get()->all();
+	 
+	  return response()->view('site.sitemap.index', [
+	      'empreendimentos' => $empreendimentos,
+	  ])->header('Content-Type', 'text/xml');
+	}
+}
---
+++ app/Http/Controllers/Site/PropostaController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Unidade;
+use App\Models\Empreendimento;
+use App\Models\Cliente;
+use App\Models\Garagem;
+use App\Models\Proposta;
+use App\Models\PropostaVaga;
+use App\Models\TabelaVendas;
+
+class PropostaController extends Controller
+{
+    public function GravarDadosCliente(Request $request){
+
+        $unidade = Unidade::find($request->unidade_id);
+        $this->data['unidade'] = $unidade;
+        $this->data['empreendimento'] = Empreendimento::find($unidade->empreendimento_id);
+        $this->data['cliente'] = (new Cliente())->salvar($request);
+        $this->data['tabela'] = TabelaVendas::where('empreendimento_id', $unidade->empreendimento_id)->where('tipo_tabela_id', 1)->first();
+        return view('site.empreendimento.premium.mobile.proposta.index', $this->data);
+
+    }
+
+    public function GravarDadosProposta(Request $request){
+
+        $unidade = Unidade::find($request->unidade_id);
+        $this->data['unidade'] = $unidade;
+        $this->data['empreendimento'] = Empreendimento::find($unidade->empreendimento_id);
+        $this->data['proposta'] = (new Proposta())->SalvarProposta($request);
+        $this->data['tabela'] = TabelaVendas::where('empreendimento_id', $unidade->empreendimento_id)->where('tipo_tabela_id', 1)->first();
+
+        if($this->data['empreendimento']->tipo == 'Vertical'){
+            $garagem = PropostaVaga::where('proposta_id', $this->data['proposta']->id)->get();
+            $this->data['garagens'] = $garagem;
+            
+            $this->data['vagas'] = Garagem::where('empreendimento_id', $unidade->empreendimento_id)->get();
+            return view('site.empreendimento.premium.mobile.proposta.selecionar_vaga', $this->data);
+        }else{
+            return view('site.empreendimento.premium.mobile.proposta.conferir', $this->data);
+        }
+
+    }
+
+    public function GravarVagaProposta(Request $request){
+
+        $proposta = Proposta::find($request->id);
+        $unidade = Unidade::find($proposta->unidade_id);
+        $this->data['proposta'] = $proposta;
+        $this->data['unidade'] = $unidade;
+        $this->data['empreendimento'] = Empreendimento::find($proposta->empreendimento_id);
+        $this->data['tabela'] = TabelaVendas::where('empreendimento_id', $proposta->empreendimento_id)->where('tipo_tabela_id', 1)->first();
+        $this->data['vaga'] = (new PropostaVaga())->salvarVaga($request, $proposta);
+
+        $garagem = PropostaVaga::where('proposta_id', $proposta->id)->get();
+        $this->data['garagens'] = $garagem;
+        $this->data['vagas'] = Garagem::where('empreendimento_id', $unidade->empreendimento_id)->get();
+
+        return view('site.empreendimento.premium.mobile.proposta.selecionar_vaga', $this->data);            
+
+    }
+
+    public function RemoverVagaProposta(Request $request){
+
+        $VagaExcluir = PropostaVaga::where('garagem_id', $request->vaga_id)->where('proposta_id', $request->proposta_id);
+        $VagaExcluir->delete();
+
+        $proposta = Proposta::find($request->proposta_id);
+        $this->data['proposta'] = $proposta;
+        $this->data['unidade'] = Unidade::find($proposta->unidade_id);
+        $this->data['empreendimento'] = Empreendimento::find($proposta->empreendimento_id);
+        $this->data['tabela'] = TabelaVendas::where('empreendimento_id', $proposta->empreendimento_id)->where('tipo_tabela_id', 1)->first();
+        
+        $garagem = PropostaVaga::where('proposta_id', $proposta->id)->get();
+        $this->data['garagens'] = $garagem;
+        $this->data['vagas'] = Garagem::where('empreendimento_id', $proposta->empreendimento_id)->get();
+
+        return view('site.empreendimento.premium.mobile.proposta.selecionar_vaga', $this->data);          
+         
+
+    }
+
+    public function ConferirProposta($id){
+
+        $proposta = Proposta::find($id);
+        $this->data['unidade'] = $proposta->unidade;
+        $this->data['empreendimento'] = Empreendimento::find($proposta->empreendimento_id);
+        $this->data['proposta'] = $proposta;
+        $this->data['tabela'] = TabelaVendas::where('empreendimento_id', $proposta->empreendimento_id)->where('tipo_tabela_id', 1)->first();
+        return view('site.empreendimento.premium.mobile.proposta.conferir', $this->data);
+         
+
+    }
+
+    public function BuscarVaga(Request $request){
+
+        $proposta = Proposta::find($request->id);
+        $this->data['proposta'] = $proposta;
+        $this->data['unidade'] = Unidade::find($proposta->unidade_id);
+        $this->data['empreendimento'] = Empreendimento::find($proposta->empreendimento_id);
+        $this->data['tabela'] = TabelaVendas::where('empreendimento_id', $proposta->empreendimento_id)->where('tipo_tabela_id', 1)->first();
+
+        $garagem = Garagem::where('unidade_id', $proposta->unidade_id)->get();
+        $this->data['garagens'] = $garagem;
+
+        if($request->vaga <> ''){
+            $this->data['vagas'] = Garagem::where('nome', 'like', '%'.$request->vaga.'%')->where('empreendimento_id', $proposta->empreendimento_id)->get();
+        }else{
+            $this->data['vagas'] = Garagem::where('empreendimento_id', $proposta->empreendimento_id)->get();
+        }
+        
+        return view('site.empreendimento.premium.mobile.proposta.selecionar_vaga', $this->data);          
+
+    }
+
+    public function AtualizarDadosProposta(Request $request){
+
+        $unidade = Unidade::find($request->unidade_id);
+        $this->data['unidade'] = $unidade;
+        $this->data['empreendimento'] = Empreendimento::find($unidade->empreendimento_id);
+        $this->data['proposta'] = (new Proposta())->AtualizarProposta($request);
+        $this->data['tabela'] = TabelaVendas::where('empreendimento_id', $unidade->empreendimento_id)->where('tipo_tabela_id', 1)->first();
+        return view('site.empreendimento.premium.mobile.proposta.conferir', $this->data);
+
+    }
+
+    public function EnviarProposta(Request $request){
+        $proposta = Proposta::find($request->proposta_id);
+        $this->data['proposta'] = $proposta;
+        $this->data['empreendimento'] = Empreendimento::find($proposta->empreendimento_id);
+        $this->data['tabela'] = TabelaVendas::where('empreendimento_id', $proposta->empreendimento_id)->where('tipo_tabela_id', 1)->first();
+        (new Proposta())->AtualizaPreferencias($request);
+
+        (new Proposta())->enviarEmails($proposta);
+
+        return view('site.empreendimento.premium.mobile.proposta.finalizar', $this->data);
+    }
+
+    public function EditarProposta($id){
+        $proposta = Proposta::find($id);
+        $this->data['unidade'] = Unidade::find($proposta->unidade_id);
+        $this->data['cliente'] = Cliente::find($proposta->cliente_id);
+        $this->data['proposta'] = $proposta;
+        $this->data['empreendimento'] = Empreendimento::find($proposta->empreendimento_id);
+        $this->data['tabela'] = TabelaVendas::where('empreendimento_id', $proposta->empreendimento_id)->where('tipo_tabela_id', 1)->first();
+        return view('site.empreendimento.premium.mobile.proposta.index', $this->data);
+    }
+
+}
