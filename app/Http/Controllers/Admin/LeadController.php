0d6082b149a97c047087aa3b8e070bed72bd02a0
Lançamentos Online <web@lancamentosonline.com.br>
2022-02-21 12:13:10 -0400

    Reenvio Lead

--- app/Http/Controllers/Admin/LeadController.php
+++ app/Http/Controllers/Admin/LeadController.php
@@ -191,27 +191,12 @@ class LeadController extends Controller
     public function TestarEnvio($lead){
         $lead = Lead::find($lead);
         Mail::to('edsongaldino@outlook.com')->send(new EmailConstrutora($lead));
-    }
 
-    public function ReenviarLeads(){
-
-        $leads = Lead::where('created_at','>', '2022-02-10 00:00:01')->limit(1)->get();
-
-        foreach($leads as $lead){
-            /*$contatos_construtora = $lead->construtora->usuarios->toArray();
+        $contatos_construtora = $lead->construtora->usuarios->toArray();
         
-            if ($contatos_construtora) {
-                $destinatarios = array_column($contatos_construtora, 'email');
-                Mail::to($destinatarios)->send(new EmailConstrutora($lead));
-            }*/
-
-            if (config('app.ambiente') == 'producao') {
-                $adms = [];
-                $adms[] = 'edson@lancamentosonline.com.br';
-                $adms[] = 'contato@lancamentosonline.com.br';
-                Mail::to($adms)->send(new EmailAdm($lead));    
-            }  
-
+        if ($contatos_construtora) {
+            $destinatarios = array_column($contatos_construtora, 'email');
+            Mail::to($destinatarios)->send(new EmailConstrutora($lead));
         }
     }
 
