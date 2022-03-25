73323e2b05d01d9dee50c0e79ce5f6af2ad400d3
Lançamentos Online <web@lancamentosonline.com.br>
2022-03-25 11:41:38 -0400

    correção erros

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -12,5 +12,5 @@
   @php
     $default_error_message = "The server is overloaded or down for maintenance. Please try again later.";
   @endphp
-  {!! $default_error_message !!}
+  {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
 @endsection
