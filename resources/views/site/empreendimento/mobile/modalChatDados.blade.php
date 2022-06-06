b417ef832d6c674c368e8c7a3d01839a83ed86a4
Edson Galdino <edsongaldino@outlook.com>
2022-06-06 16:57:29 -0400

    Ajustes Telefone Construtora

--- resources/views/site/empreendimento/mobile/modalChatDados.blade.php
+++ resources/views/site/empreendimento/mobile/modalChatDados.blade.php
@@ -25,7 +25,9 @@
         <button type="button" class="btn btn-primary btn-whatsapp"><i class="fa fa-whatsapp" aria-hidden="true"></i> Atendimento Whatsapp</button>
       </a>
       @else
+      <a href="https://api.whatsapp.com/send?phone=5565999859238&text=Ol%C3%A1%2C%20vi%20o%20an%C3%BAncio%20do%20empreendimento%20({{ $empreendimento->nome }})%20no%20Portal%20Lan%C3%A7amentos%20Online%20e%20gostaria%20de%20obter%20maiores%20informa%C3%A7%C3%B5es" target="_blank">
         <button type="button" class="btn btn-primary btn-whatsapp"><i class="fa fa-whatsapp" aria-hidden="true"></i> Atendimento Whatsapp</button>
+      </a>
       @endif
       </div>
 
@@ -48,7 +50,9 @@
           <button type="button" class="btn btn-primary btn-ligar"><i class="fa fa-phone" aria-hidden="true"></i> Ligar para a construtora</button>
         </a>
         @else
+        <a href="tel:65999859238" onclick="GravarCliquetel(); return true;">
           <button type="button" class="btn btn-primary btn-ligar-off"><i class="fa fa-phone" aria-hidden="true"></i> Ligar para a construtora</button>
+        </a>
         @endif
 
       </div>
