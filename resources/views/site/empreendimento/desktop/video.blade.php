e8ece4692c47c6cd540011adc5a155264b45cf84
Edson Galdino <edsongaldino@outlook.com>
2022-06-23 15:15:53 -0400

    Ajustes Vídeo

--- resources/views/site/empreendimento/desktop/video.blade.php
+++ resources/views/site/empreendimento/desktop/video.blade.php
@@ -1,19 +1,24 @@
 @if($empreendimento->arquivos->where('tipo', 'Vídeo')->count() > 0)
 <div class="row box-video-detalhes">
-  <div class="container">
-    <div class="row margin-top-30">
-      <div class="col-xs-10">
-        <h3 class="title-negative-margin">Vídeo do Empreendimento</h3>
-        <div class="title-separator-primary"></div>
-      </div>
-    </div>
-  </div>
+    <div class="col-xs-12 col-sm-9 margin-top-60">
+        <h2 class="title-negative-margin title-color">
+            <i class="fa fa-video-camera" aria-hidden="true"></i> Vídeo
+        </h2>
+        <h5 class="subtitle-color">
+            Vídeo de apresentação do empreendimento
+        </h5>
 
-  <div class="container gallery-filter-cont margin-top-60">
-    <div class="row">
-        <video src="{{ $empreendimento->arquivos->where('tipo', 'Vídeo')->first()->arquivo }}" controls></video>
+        <div class="col-xs-12">
+            <div class="title-separator-secondary"></div>
+        </div>
+
     </div>
-  </div>
+
+    <div class="container margin-top-80">
+        <div class="row">
+            <video src="{{ $empreendimento->arquivos->where('tipo', 'Vídeo')->first()->arquivo }}" controls></video>
+        </div>
+    </div>
 
 </div>
 @endif
