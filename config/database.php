14ef3156446f958d20cda425eb7a246c679e7a6a
Edson Galdino <edsongaldino@outlook.com>
2023-04-13 10:45:49 -0400

    Alteração database

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
             'prefix_indexes' => true,
             'strict' => false,
             'engine' => null,
-            'options' => array_filter([
+            /*'options' => array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]),
-        ],        
+            ]),*/
+        ],
         'banco_lancamentos' => [
             'driver' => 'mysql',
             'host' => 'lanca_bkp.mysql.dbaas.com.br',
@@ -72,9 +72,9 @@ return [
             'prefix_indexes' => true,
             'strict' => false,
             'engine' => null,
-            'options' => array_filter([
+            /*'options' => array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]),
+            ]),*/
         ],
         'pgsql' => [
             'driver' => 'pgsql',
