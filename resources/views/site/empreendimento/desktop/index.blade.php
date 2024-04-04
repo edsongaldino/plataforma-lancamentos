d0032eb5ff5cd94ea2dfc3f6707361ed83736d8d
Edson Galdino <edsongaldino@outlook.com>
2024-04-04 10:41:16 -0400

    ajuste pixel facebiook

--- resources/views/site/empreendimento/desktop/index.blade.php
+++ resources/views/site/empreendimento/desktop/index.blade.php
@@ -65,21 +65,6 @@
   gtag('event', 'conversion', {'send_to': 'AW-791268609/XYF3COGT5OsBEIGap_kC'});
 </script>
 
-@if($empreendimento->construtora_id == 58)
-  <script>
-  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-  document,'script','https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '576443426940162'); // Insert your pixel ID here.
-  fbq('track', 'PageView');
-  </script>
-  <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=576443426940162&ev=PageView&noscript=1"
-  /></noscript>
-@endif
-
 @endpush
 
 @push('js_footer')
@@ -92,6 +77,21 @@
 @section('content')
   @include('site/empreendimento/desktop/modal')
   @include('site/empreendimento/desktop/carrossel')
+
+  @if($empreendimento->construtora_id == 58)
+  <script>
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+    n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+    document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '576443426940162'); // Insert your pixel ID here.
+    fbq('track', 'PageView');
+  </script>
+  <noscript>
+    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=576443426940162&ev=PageView&noscript=1"/>
+  </noscript>
+  @endif
 
   <section class="section-light no-bottom-padding">
     <div class="container">
